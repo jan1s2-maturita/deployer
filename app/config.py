972f2a7165185876170a4be17df0dd58d55d201c
Jan Str <janstr06@gmail.com>
2024-11-17 22:07:30 +0100

    update config

--- app/config.py
+++ app/config.py
@@ -1,16 +1,16 @@
 import os
-DEBUG = True
-DEPLOY_NAMESPACE = 'dev' if DEBUG else os.environ.get('DEPLOY_NAMESPACE')
-KUBERNETES_KEY = 'k8s_key' if DEBUG else os.environ.get('KUBERNETES_KEY')
-KUBERNETES_URL = 'https://kubernetes.default.svc.cluster.local'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEPLOY_NAMESPACE = os.environ.get('DEPLOY_NAMESPACE', 'dev')
+KUBERNETES_KEY = os.environ.get('KUBERNETES_KEY', 'k8s_key')
+KUBERNETES_URL = os.environ.get('KUBENETES_URL', 'https://kubernetes.default.svc.cluster.local')
 
-PUBLIC_KEY = 'public.pem' if DEBUG else os.environ.get('PUBLIC_KEY')
+PUBLIC_KEY_PATH = os.environ.get('PUBLIC_KEY_PATH', 'public.pem')
 
-DB_HOST = 'db' if DEBUG else os.environ.get('DB_HOST')
-DB_PORT = 5432 if DEBUG else os.environ.get('DB_PORT')
-DB_USER = 'postgres' if DEBUG else os.environ.get('DB_USER')
-DB_PASS = 'postgres' if DEBUG else os.environ.get('DB_PASS')
-DB_NAME = 'postgres' if DEBUG else os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST', 'db')
+DB_PORT = int(os.environ.get('DB_PORT', '5432'))
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PASS = os.environ.get('DB_PASS', 'postgres')
+DB_NAME = os.environ.get('DB_NAME', 'postgres')
 
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
