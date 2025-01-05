71900e44248c88d7cd50e0f6d1ed67722e99694c
Jan Str <janstr06@gmail.com>
2025-01-05 20:03:15 +0100

    update

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 KUBERNETES_KEY = os.environ.get('KUBERNETES_KEY', 'k8s_key')
-KUBERNETES_URL = os.environ.get('KUBENETES_URL', 'https://kubernetes.default.svc.cluster.local')
+KUBERNETES_URL = os.environ.get('KUBERNETES_URL', 'https://kubernetes.default.svc.cluster.local')
 
 PUBLIC_KEY_PATH = os.environ.get('PUBLIC_KEY_PATH', 'public.pem')
 
